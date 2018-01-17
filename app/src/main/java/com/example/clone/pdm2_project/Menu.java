ce6419b1a96dc723dde5ffeab70b33572b98c217
clone <clone1456@hotmail.com>
2018-01-17 21:47:45 +0000

    Fase5 animaçoes

--- app/src/main/java/com/example/clone/pdm2_project/Menu.java
+++ app/src/main/java/com/example/clone/pdm2_project/Menu.java
@@ -1,18 +1,68 @@
 package com.example.clone.pdm2_project;
 
 import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.view.ViewGroup;
 
 import info.androidhive.androidsplashscreentimer.R;
 
 public class Menu extends AppCompatActivity {
 
+    boolean firstScene;
+    ViewGroup mSceneRoot;
+    Scene mAScene;
+    Scene mAnotherScene;
+    Transition mFadeTransition;
+    Runnable runnable;
+    private Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+        handler.postDelayed(runnable, 1000);
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if(firstScene == true) {
+                    TransitionManager.go(mAnotherScene, mFadeTransition);
+                    firstScene = false;
+                }else{
+                    TransitionManager.go(mAScene, mFadeTransition);
+                    firstScene = true;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        handler.postDelayed(runnable, 100);
+        // Create the scene root for the scenes in this app
+        mSceneRoot = (ViewGroup) findViewById(R.id.scene_root);
+
+        // Create the
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            mAScene = Scene.getSceneForLayout(mSceneRoot, R.layout.scene1, this);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            mAnotherScene =
+                    Scene.getSceneForLayout(mSceneRoot, R.layout.scene2, this);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            mFadeTransition =
+                    TransitionInflater.from(this).
+                            inflateTransition(R.transition.fade_transition);
+        }
+        firstScene = true;
     }
 
     public void receitas(View view) {
